30a7c26e40a8002d6285f46474bffa332aa0a10a
jhnnsrs <jhnnsrs@gmail.com>
2024-01-24 18:48:37 +0100

    up party

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from kluster.deployed import DeployedKluster, deployed
 from typing import Iterator
+from koil import unkoil
+import logging
 
 
 @pytest.fixture(scope="session")
@@ -8,7 +10,16 @@ def deployed_app() -> Iterator[DeployedKluster]:
     """A deployed kluster"""
     app = deployed()
     app.deployment.project.overwrite = True
-    app.deployment.health_on_enter = True
     app.deployment.down_on_exit = True
     with app:
+        logging.warning("Deploying app")
+        app.deployment.down()
+        app.deployment.up()
+        logging.warning("Deployed app. Waiting for healthz")
+        app.deployment.wait_for_healthz()
+        logging.warning("Deployment is healthy")
+
+
         yield app
+
+        logging.warning("Tearing down Deployment")
